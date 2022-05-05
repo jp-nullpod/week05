8e9421462ca24050b54f613c02a252b77561ea2d
Jean Paul Fortuno <jp.fortuno@nullpod.com>
2022-05-05 20:26:49 +0400

    mods to checkboxes

--- 01-dom-events/js/form-script.js
+++ 01-dom-events/js/form-script.js
@@ -1,5 +1,5 @@
 
-let form = document.forms['survey-form'];
+let form = document.forms[0]; //document.forms['survey-form']
 
 console.log(form.elements);
 console.log(form.elements['name']);
@@ -10,3 +10,23 @@ form.elements['name'].value = "Tom Hardy";
 form.elements['number'].addEventListener("input", event => {
     console.log(event.target.value);
 });
+
+const dropdown = document.getElementById('dropdown');
+dropdown.value = 'learner';
+
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+checkboxes[1].checked = true;
+checkboxes[4].checked = true;
+
+//this won't work, only for testing
+// const checkboxList = document.getElementsByClassName('form-check-input');
+// checkboxList[3].checked = true;
+
+/* 
+const listofItems = document.getElementById('abc');
+const checkboxes = listofItems.getElementsByClassName('form-check-input');
+*/
+
+
+
+
